cd371f331b9ea9d20fc39b14eb91a033a81ecf9e
Anyelo Nuñez Obispo <anyelonuob2004@gmail.com>
2025-05-21 22:17:24 -0500

    chore: Creacion de proyecto base

---
+++ app/build.gradle.kts
@@ -0,0 +1,83 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    kotlin("kapt")
+}
+
+android {
+    namespace = "com.example.app_movil_kabj"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.app_movil_kabj"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    // Habilitar ViewBinding
+    buildFeatures {
+        viewBinding = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    // Arquitectura
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+
+    // Networking
+    implementation(libs.retrofit2.retrofit)
+    implementation(libs.retrofit2.converter.gson)
+    implementation(libs.squareup.logging.interceptor)
+
+    // Coroutines
+    implementation(libs.jetbrains.kotlinx.coroutines.android)
+
+    // Room
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    kapt(libs.room.compiler)
+
+    // Excel
+    implementation(libs.apache.poi)
+    implementation(libs.apache.poi.ooxml)
+
+    // UI
+    implementation(libs.google.material)
+    implementation(libs.constraintlayout)
+
+    implementation(libs.jakewharton.timber)
+    implementation(libs.core.ktx)
+
+}
