f64abd9f38a52b2bdd09df83d580bb6df91388c6
Anyelo Nuñez Obispo <anyelonuob2004@gmail.com>
2025-05-29 10:30:43 -0500

    bugfix: Cambiando de clase logica del login

--- app/src/main/java/pe/kabj/app_movil_kabj/MainActivity.kt
+++ app/src/main/java/pe/kabj/app_movil_kabj/MainActivity.kt
@@ -1,110 +1,22 @@
 package pe.kabj.app_movil_kabj
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.WindowInsetsControllerCompat
-import androidx.core.widget.addTextChangedListener
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
-import pe.kabj.app_movil_kabj.databinding.ActivityLoginBinding
-import pe.kabj.app_movil_kabj.extensions.validateNotEmpty
+import pe.kabj.app_movil_kabj.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityLoginBinding
-
-    //Components
-    private lateinit var layoutTxtUsername: TextInputLayout
-    private lateinit var txtUsername: TextInputEditText
-    private lateinit var layoutTxtPassword: TextInputLayout
-    private lateinit var txtPassword: TextInputEditText
-    private lateinit var btnLogin: MaterialButton
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        hideSystemBars()
 
-        setupComponents()
-        setupListeners()
-        animateLogin()
-
-    }
-
-    private fun setupComponents() {
-        this.layoutTxtUsername = binding.layoutTxtUsername
-        this.txtUsername = binding.txtUsername
-        this.layoutTxtPassword = binding.layoutTxtPassword
-        this.txtPassword = binding.txtPassword
-        this.btnLogin = binding.btnLogin
-    }
-
-    private fun setupListeners() {
-        txtUsername.addTextChangedListener {
-            if (!it.isNullOrBlank()) {
-                layoutTxtUsername.error = null
-            }
-        }
-
-        txtPassword.addTextChangedListener {
-            if (!it.isNullOrBlank()) {
-                layoutTxtPassword.error = null
-            }
-        }
-        btnLogin.setOnClickListener {
-            handleLogin()
-        }
-    }
-
-    private fun handleLogin() {
-        val isUsernameValid = layoutTxtUsername.validateNotEmpty(getString(R.string.msg_error_username_required))
-        val isPasswordValid = layoutTxtPassword.validateNotEmpty(getString(R.string.msg_error_password_required))
-
-        if (isUsernameValid && isPasswordValid) {
-            val username = txtUsername.text.toString().trim()
-            val password = txtPassword.text.toString().trim()
-
-            showToast(getString(R.string.msg_login_success) + " $username")
-        }
-    }
-
-    private fun animateLogin() {
-        binding.imageLogo.animate()
-            .alpha(1f)
-            .setDuration(800)
-            .setStartDelay(100)
-            .start()
-
-        binding.cardViewLogin.animate()
-            .alpha(1f)
-            .setDuration(1000)
-            .setStartDelay(300)
-            .start()
-    }
-
-
-    private fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
-
-    private fun hideSystemBars() {
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-
-        val controller = WindowInsetsControllerCompat(window, window.decorView)
-        controller.hide(
-            WindowInsetsCompat.Type.navigationBars() or WindowInsetsCompat.Type.statusBars()
-        )
-        controller.systemBarsBehavior =
-            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 
 }
