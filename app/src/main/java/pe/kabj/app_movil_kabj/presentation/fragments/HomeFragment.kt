a535cfea2cbdc146dd62bfa167cae222575f017f
Anyelo Nuñez Obispo <anyelonuob2004@gmail.com>
2025-06-30 19:46:59 -0500

    feat: Filtrar descripcion de funcionalidades del Inicio

--- app/src/main/java/pe/kabj/app_movil_kabj/presentation/fragments/HomeFragment.kt
+++ app/src/main/java/pe/kabj/app_movil_kabj/presentation/fragments/HomeFragment.kt
@@ -6,12 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import pe.kabj.app_movil_kabj.data.local.SessionManager
 import pe.kabj.app_movil_kabj.presentation.viewmodels.HomeViewModel
-import pe.kabj.app_movil_kabj.R
+import pe.kabj.app_movil_kabj.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
     private val viewModel: HomeViewModel by viewModels()
+
+    private lateinit var sessionManager: SessionManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +25,31 @@ class HomeFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding =  FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        sessionManager = SessionManager(requireContext())
+
+        setupObservers()
+
+        viewModel.loadVisibleCards(sessionManager.fetchPermissions())
+    }
+
+    private fun setupObservers(){
+        viewModel.visibleCards.observe(viewLifecycleOwner) { visibleCards ->
+            binding.cardRegisterWorkOrder.visibility =
+                if (visibleCards.contains("REGISTER")) View.VISIBLE else View.GONE
+            binding.cardConsultWorkOrder.visibility =
+                if (visibleCards.contains("CONSULT")) View.VISIBLE else View.GONE
+            binding.cardAssignWorkOrder.visibility =
+                if (visibleCards.contains("ASSIGN")) View.VISIBLE else View.GONE
+            binding.cardForemanWorkOrders.visibility =
+                if (visibleCards.contains("FOREMAN")) View.VISIBLE else View.GONE
+        }
+
     }
 
 }
--- app/src/main/java/pe/kabj/app_movil_kabj/presentation/viewmodels/HomeViewModel.kt
+++ app/src/main/java/pe/kabj/app_movil_kabj/presentation/viewmodels/HomeViewModel.kt
@@ -1,7 +1,23 @@
 package pe.kabj.app_movil_kabj.presentation.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _visibleCards: MutableLiveData<Set<String>> = MutableLiveData()
+    val visibleCards: LiveData<Set<String>> get() = _visibleCards
+
+    fun loadVisibleCards(userPermissions: List<String>) {
+        val visible = mutableSetOf<String>()
+
+        if (userPermissions.any { it == "MOBILE_WORK_ORDER_REGISTER_VIEW" }) visible.add("REGISTER")
+        if (userPermissions.any { it == "MOBILE_WORK_ORDER_CONSULT_VIEW" }) visible.add("CONSULT")
+        if (userPermissions.any { it == "MOBILE_WORK_ORDER_ASSIGNED_LIST_VIEW" }) visible.add("ASSIGN")
+        if (userPermissions.any { it == "MOBILE_WORK_ORDER_LIST_VIEW" }) visible.add("FOREMAN")
+
+        _visibleCards.postValue(visible)
+    }
+
 }
